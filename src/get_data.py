adb709a5fc2aa4d17ef5050095b6e699fbd36b7e
Rafael Torres <souza.t.rafael@gmail.com>
2021-10-31 11:50:31 -0300

    Fix grid value and adds url valid check

--- src/get_data.py
+++ src/get_data.py
@@ -25,7 +25,10 @@ def treat_grids(grids):
         return ''
     grid_value = ''
     for grid in grids:
-        grid_value = '{},{}'.format(grid_value, grid.replace('/api/v3/grids/', '').replace('/', ''))
+        if grid_value == '':
+            grid_value = grid.replace('/api/v3/grids/', '').replace('/', '')
+        else:
+            grid_value = '{},{}'.format(grid_value, grid.replace('/api/v3/grids/', '').replace('/', ''))
     return grid_value
 
 
@@ -51,6 +54,36 @@ def get_csv_line_by_package(package):
     )
 
 
+def check_valid_repo(repo_url):
+    import requests
+    import re
+
+    regex = re.compile(
+        r'^(?:http|ftp)s?://' # http:// or https://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+        r'localhost|' #localhost...
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+        r'(?::\d+)?' # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+
+    if not re.match(regex, repo_url):
+        return False
+
+    valid_domains = ['github', 'bitbucket', 'gitlab', 'google.code']
+    if len([d for d in valid_domains if d in repo_url.lower()]) == 0:
+        return False
+
+    try:
+        test_response = requests.get(repo_url)
+        test_response.raise_for_status()
+    except requests.models.ConnectionError:
+        return False
+    except requests.models.HTTPError:
+        return False
+    
+    return True
+
+
 if __name__ == '__main__':
     import os
 
@@ -65,4 +98,5 @@ if __name__ == '__main__':
         packages_remaining = len(next) > 0
         with open('django-packages.csv', 'a') as f:
             for package in packages:
-                f.write(get_csv_line_by_package(package) + '\n')
+                if check_valid_repo(package['repo_url']):
+                    f.write(get_csv_line_by_package(package) + '\n')

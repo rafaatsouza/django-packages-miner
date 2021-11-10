28de28b11a509aec746e3af60dec354ac0fe99c1
Rafael Torres <souza.t.rafael@gmail.com>
2021-11-09 21:40:27 -0300

    Extracts logic into separated class

--- src/main.py
+++ src/main.py
@@ -1,107 +1,8 @@
-import requests
-import re
 import os
 
-from control_version import Platform
 from django_packages import DjangoPackagesApi
-
-def check_valid_repo_url(repo_url):
-    regex_domains = ''
-    for domain in Platform.get_valid_domains():
-        if regex_domains == '':
-            regex_domains = domain
-        else:
-            regex_domains = '{}|{}'.format(regex_domains, domain)
-
-    regex = re.compile(
-        r'^(http(s){0,1}\:\/\/){0,1}((www\.){0,1})'
-        r'((' + regex_domains + r')\.com\/)'
-        r'([a-zA-Z0-9_.-]+\/([a-zA-Z0-9_.-]+\/)?[a-zA-Z0-9_.-]+)'
-        r'((\/[a-zA-Z0-9_.-\/]+)|\Z)', re.IGNORECASE)
-
-    if not re.match(regex, repo_url):
-        return False
-
-    try:
-        test_response = requests.get(repo_url)
-        test_response.raise_for_status()
-    except requests.models.ConnectionError:
-        return False
-    except requests.models.HTTPError:
-        return False
-    
-    return True
-
-
-def get_token(platform, tokens):
-    if platform.value == Platform.GITHUB.value:
-        return tokens['github']
-    elif platform.value == Platform.GITLAB.value:
-        return tokens['gitlab']
-
-
-def get_csv_line_by_package(columns, package, tokens):
-    valid_repo_url = check_valid_repo_url(package['repo_url'])
-    line = '{};{};{};{}'.format(
-        package['slug'],
-        package['category'],
-        package['grids'],
-        package['usage_count'] or 0
-    )
-
-    if not valid_repo_url:
-        line = '{};{};{};{}'.format(line, False, '', False)
-
-        for _ in range(len(columns) - 7):
-            line = '{};{}'.format(line, '')
-        
-        return line
-
-
-    platform = Platform.get_platform(package['repo_url'])
-    token = get_token(platform, tokens)
-    repo_info = platform.get_repo_info(package['repo_url'], token)
-
-    if not repo_info:
-        line = '{};{};{};{};{}'.format(
-            line, 
-            True, 
-            package['repo_url'], 
-            False, 
-            platform.domain
-        )
-
-        for _ in range(len(columns) - 8):
-            line = '{};{}'.format(line, '')
-        
-        return line
-
-    line = '{};{};{};{};{};{};{};{};{};{};{}'.format(
-        line, 
-        True, 
-        package['repo_url'], 
-        True, 
-        platform.domain,
-        repo_info['repo_id'], 
-        repo_info['repo_stars'],     
-        repo_info['repo_last_modified'], 
-        repo_info['repo_forks'], 
-        repo_info['repo_open_issues'], 
-        repo_info['repo_topics']
-    )
-    
-    return line
-
-
-def write_package_csv_line(output_file, package, columns, tokens, logger):
-    with open(output_file, 'a') as f:
-        try:
-            f.write(get_csv_line_by_package(columns, package, tokens) + '\n')
-        except BaseException as error:
-            logger.error('Unknown error at package {}'.format(package['slug']), error)
-            return False
-
-    return True
+from report_register import ReportRegister
+from control_version import Platform
 
 
 def get_logger(log_file_name):
@@ -120,16 +21,6 @@ def get_logger(log_file_name):
     return logger
 
 
-def get_header_line(columns):
-    line = ''
-    for column in columns:
-        if line == '':
-            line = column
-        else:
-            line = '{};{}'.format(line, column)
-    return line
-
-
 if __name__ == '__main__':
     logger = get_logger('../data/{}'.format(os.environ.get('LOG_FILE_NAME') or 'file.log'))
     
@@ -140,17 +31,14 @@ if __name__ == '__main__':
 
     output_file = '../data/{}'.format(os.environ.get('OUTPUT_FILE') or 'data.csv')
 
-    # TODO: add language
-    columns = [
-        'dp_slug', 'dp_category', 'dp_grids', 'dp_usage_count', 'has_valid_repo_url', 
-        'dp_repo_url', 'has_valid_repo', 'platform', 'repo_id', 'repo_stars', 
-        'repo_last_modified', 'repo_forks', 'repo_open_issues', 'repo_topics'
-    ]
-
     with open(output_file, 'a') as f:
-        f.write('{}\n'.format(get_header_line(columns)))
+        f.write('{}\n'.format(ReportRegister.get_header_line()))
 
     django_packages_api = DjangoPackagesApi()
+    succeded_packages = {
+        Platform.GITHUB.domain: [],
+        Platform.GITLAB.domain: [],
+    }
     failed_packages = []
     packages_remaining = True
     next = None
@@ -159,9 +47,25 @@ if __name__ == '__main__':
         packages, next = django_packages_api.get_packages(next)
         packages_remaining = len(next) > 0
         for package in packages:
-            if not write_package_csv_line(output_file, package, columns, tokens, logger):
+            try:
+                register = ReportRegister(package, tokens)
+                if not register.has_valid_repo or register.repo_id not in succeded_packages[register.platform]:
+                    with open(output_file, 'a') as f:
+                        f.write(register.get_line() + '\n')
+                    if register.has_valid_repo and register.repo_id not in succeded_packages[register.platform]:
+                        succeded_packages[register.platform].append(register.repo_id)
+            except BaseException as error:
+                logger.exception('Unknown error at package {}'.format(package['slug']))
                 failed_packages.append(package['slug'])
 
     for package_id in failed_packages:
         package = django_packages_api.get_package(package_id)
-        write_package_csv_line(output_file, package, columns, tokens, logger)
+        try:
+            register = ReportRegister(package, tokens)
+            if not register.has_valid_repo or register.repo_id not in succeded_packages[register.platform]:
+                with open(output_file, 'a') as f:
+                    f.write(register.get_line() + '\n')
+                if register.has_valid_repo and register.repo_id not in succeded_packages[register.platform]:
+                        succeded_packages[register.platform].append(register.repo_id)
+        except BaseException as error:
+            logger.exception('Unknown error at package {}'.format(package['slug']))
---
+++ src/report_register.py
@@ -0,0 +1,116 @@
+from control_version import Platform
+
+
+def _check_valid_repo_url(repo_url):
+    import requests
+    import re
+
+    regex_domains = ''
+    for domain in Platform.get_valid_domains():
+        if regex_domains == '':
+            regex_domains = domain
+        else:
+            regex_domains = '{}|{}'.format(regex_domains, domain)
+
+    regex = re.compile(
+        r'^(http(s){0,1}\:\/\/){0,1}((www\.){0,1})'
+        r'((' + regex_domains + r')\.com\/)'
+        r'([a-zA-Z0-9_.-]+\/([a-zA-Z0-9_.-]+\/)?[a-zA-Z0-9_.-]+)'
+        r'((\/[a-zA-Z0-9_.-\/]+)|\Z)', re.IGNORECASE)
+
+    if not re.match(regex, repo_url):
+        return False
+
+    try:
+        test_response = requests.get(repo_url)
+        test_response.raise_for_status()
+    except requests.models.ConnectionError:
+        return False
+    except requests.models.HTTPError:
+        return False
+    
+    return True
+
+
+class ReportRegister():
+    _COLUMNS = [
+        'dp_slug', 'dp_category', 'dp_grids', 'dp_usage_count', 'has_valid_repo_url', 
+        'dp_repo_url', 'has_valid_repo', 'platform', 'repo_id', 'repo_stars', 
+        'repo_last_modified', 'repo_forks', 'repo_open_issues', 'repo_topics'
+    ]
+
+    @staticmethod
+    def get_header_line():
+        return (
+            'dp_slug;dp_category;dp_grids;dp_usage_count;has_valid_repo_url;dp_repo_url;has_valid_repo;'
+            'platform;repo_id;repo_stars;repo_last_modified;repo_forks;repo_open_issues;repo_topics'
+        )
+
+
+    def __init__(self, package, tokens):
+        self.slug = package['slug']
+        self.category = package['category']
+        self.grids = package['grids']
+        self.usage_count = package['usage_count'] or 0
+        self.has_valid_repo_url = _check_valid_repo_url(package['repo_url'])
+        self.repo_url = package['repo_url'] if self.has_valid_repo_url else ''
+        self.has_valid_repo = False
+
+        if self.has_valid_repo_url:            
+            platform = Platform.get_platform(package['repo_url'])
+            self.platform = platform.domain
+
+            token = self._get_token(platform, tokens)
+            repo_info = platform.get_repo_info(package['repo_url'], token)
+
+            if repo_info:
+                self.has_valid_repo = True
+                self.repo_id = repo_info['repo_id']
+                self.repo_stars = repo_info['repo_stars']
+                self.repo_last_modified = repo_info['repo_last_modified']
+                self.repo_forks = repo_info['repo_forks']
+                self.repo_open_issues = repo_info['repo_open_issues']
+                self.repo_topics = repo_info['repo_topics']
+            
+
+    def get_line(self):
+        line = '{};{};{};{};{};{};{}'.format(
+            self.slug, 
+            self.category, 
+            self.grids, 
+            self.usage_count, 
+            self.has_valid_repo_url,
+            self.repo_url,
+            self.has_valid_repo
+        )
+
+        if not self.has_valid_repo_url:
+            for _ in range(len(self._COLUMNS) - 7):
+                line = '{};{}'.format(line, '')
+            
+            return line
+
+        line = '{};{}'.format(line, self.platform)
+
+        if not self.has_valid_repo:
+            for _ in range(len(self._COLUMNS) - 8):
+                line = '{};{}'.format(line, '')
+            
+            return line
+
+        return '{};{};{};{};{};{};{}'.format(
+            line, 
+            self.repo_id, 
+            self.repo_stars,     
+            self.repo_last_modified, 
+            self.repo_forks, 
+            self.repo_open_issues, 
+            self.repo_topics
+        )
+
+
+    def _get_token(self, platform, tokens):
+        if platform.value == Platform.GITHUB.value:
+            return tokens['github']
+        elif platform.value == Platform.GITLAB.value:
+            return tokens['gitlab']
